e9daea3d365b5792a550fa2cea5f2010e9c19bd1
viv327 <jingzhang@jings-mbp.mynetworksettings.com>
2023-07-28 11:41:49 -0400

    first commit example

---
+++ sparc_busy_baby/aws_codepipeline_stack.py
@@ -0,0 +1,60 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    # aws_sqs as sqs,
+    Stage,
+    Environment,
+    pipelines,
+    aws_codepipeline as codepipeline
+)
+from constructs import Construct
+from resource_stack.resource_stack import ResourceStack
+
+
+class DeployStage(Stage):
+    def __init__(self, scope: Construct, id: str, env: Environment, **kwargs) -> None:
+        super().__init__(scope, id, env=env, **kwargs)
+        ResourceStack(self, 'ResourceStack', env=env, stack_name="resource-stack-deploy")
+
+
+class AwsCodepipelineStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        git_input = pipelines.CodePipelineSource.connection(
+            repo_string="viv327/sparc-busy-baby",
+            branch="main",
+            connection_arn="arn:aws:codestar-connections:us-east-1:311242152394:connection/98b893e3-4c7b-44f5-a0a4-385ff76e14f2"
+        )
+
+        code_pipeline = codepipeline.Pipeline(
+            self, "Pipeline",
+            pipeline_name="new-pipeline",
+            cross_account_keys=False
+        )
+
+        synth_step = pipelines.ShellStep(
+            id="Synth",
+            install_commands=[
+                'pip install -r requirements.txt'
+            ],
+            commands=[
+                'npx cdk synth'
+            ],
+            input=git_input
+        )
+
+        pipeline = pipelines.CodePipeline(
+            self, 'CodePipeline',
+            self_mutation=True,
+            code_pipeline=code_pipeline,
+            synth=synth_step
+        )
+
+        deployment_wave = pipeline.add_wave("DeploymentWave")
+
+        deployment_wave.add_stage(DeployStage(
+            self, 'DeployStage',
+            env=(Environment(account='311242152394', region='us-east-1'))
+        ))
